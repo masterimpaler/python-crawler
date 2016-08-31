a2aff82dd55e2621001570b3e3c3cc2aece3100d
masterimpaler <loughlinjon@gmail.com>
2016-08-31 14:54:34 +0300

    Made subtle changes

--- PHP version/crawler/index.php
+++ PHP version/crawler/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once('simple_html_dom.php');
 $html = new simple_html_dom();
-$target_url = "http://www.jkuat.ac.ke";
+$target_url = "http://www.example.com";
 $html->load_file($target_url);
 foreach($html->find('a') as $link)
 {
